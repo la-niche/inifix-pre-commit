911a17515e569f9014c823a70131a1a01d51e173
Clément Robert <cr52@protonmail.com>
2026-05-17 18:45:36 +0200

    CI: setup hooks and CI

--- src/inifix_pre_commit/__init__.py
+++ src/inifix_pre_commit/__init__.py
@@ -1,1 +1,1 @@
-raise ImportError("This package isn't meant to be installed from PyPI. See https://github.com/la-niche/inifix-pre-commit/")
+raise ImportError("inifix_pre_commit is a metapackage, not meant to be used by itself")
